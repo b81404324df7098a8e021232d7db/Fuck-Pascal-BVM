9337d168d2c515c288461ddd67a65de1f000ac26
wan-qy <wan-qy@users.noreply.github.com>
2016-07-29 01:00:28 +0800

    如何处理循环不会写，解释器搁置。

--- interpreter.pas
+++ interpreter.pas
@@ -1,1 +1,29 @@
-
+var 
+    ch:char;
+    c:array[1..100000] of byte;
+    pointer:longint;
+    tmp:char;
+begin
+    assign(input,'source.bf');
+    assign(output,'source.pas');
+    reset(input);
+    rewrite(output);
+    fillchar(c,sizeof(c),0);
+    pointer:=1;
+    while ch<>'$' do
+    begin
+        read(ch);
+        case ch of
+            '>':inc(pointer);
+            '<':dec(pointer);
+            '+':inc(c[pointer]);
+            '-':dec(c[pointer]);
+            '.':wwrite(chr(c[pointer]));
+            ',':begin read(tmp); c[pointer]:=ord(tmp); end;
+            '[':begin while ord(c[pointer])<>0 do writeln('begin'); end;
+            ']':writeln('end;');
+        end;
+    end;
+    close(input);
+    close(output);
+end.
